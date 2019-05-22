ff29d48c59934b22011494df4012efa12d2b6095
Caina C. Caldas <cainacc@hotmail.com>
2019-05-22 09:01:52 -0300

    primeiro prototipo

---
+++ Game Financeiro/Assets/Click.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Click : MonoBehaviour
+{
+
+    bool metronEnabled = false;
+    public Text vezesTxt;
+    int vezes = 10;
+
+    float angles = 15f;
+    public Transform picareta;
+    float vel = 15;
+    float startAngle;
+
+    public GameObject metronomo;
+
+    public GameObject nada, bom, excelente, vish;
+
+    void Start()
+    {
+        startAngle = picareta.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            metronEnabled = !metronEnabled;
+            metronomo.SetActive(metronEnabled);
+            GetComponent<Animator>().SetTrigger("change");
+            if (metronEnabled) {
+                metronomo.transform.position = new Vector3(metronomo.transform.position.x , -4.2f , 0);
+                vezes--;
+                vezesTxt.text = ""+vezes;
+                vezesTxt.GetComponent<Animator>().SetTrigger("blink");
+                StartCoroutine("IdlePicareta");
+            } else {
+                float y = metronomo.transform.position.y;
+                if (y < .5f) {
+                    nada.SetActive(true);
+                } else if (y < 1.5f) {
+                    bom.SetActive(true);
+                } else if (y < 2.5f) {
+                    excelente.SetActive(true);
+                } else {
+                    vish.SetActive(true);
+                }
+
+            }
+
+        }
+    }
+
+    IEnumerator IdlePicareta () {
+        float nowAngle = 0;
+
+        while (metronEnabled) {
+            nowAngle += vel*Time.deltaTime;
+            if (nowAngle > angles) {
+                nowAngle = angles - 0.1f;
+                vel = -vel;
+            }
+
+            if (nowAngle < 0) {
+                nowAngle = 0.1f;
+                vel = -vel;
+            }
+
+            picareta.eulerAngles = new Vector3(0 , 0 , startAngle+nowAngle);
+            yield return null;
+        }
+        picareta.eulerAngles = new Vector3(0 , 0 , 0);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(0.1f);
+        picareta.eulerAngles = new Vector3(0 , 0 , startAngle);
+    }
+
+
+}
---
+++ Game Financeiro/Assets/barrinha.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrinha : MonoBehaviour
+{
+    float vel = 8   ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0,vel*Time.deltaTime, 0);
+        if (transform.position.y > 3.5)
+            transform.position = new Vector3(transform.position.x , -4.17f , 0);
+    }
+}
